ac97e1630577fd695819fc0307b65200214767a2
Sergio <sergio.marretta@libero.it>
2021-03-15 17:07:34 +0100

    working

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,8 @@
-$("main-nav li").click(function() {
+$(".main-nav li").mouseenter(function() {
 $(this).children(".dropdown-menu").show();
+
+});
+$(".main-nav li").click(function() {
+$(this).children(".dropdown-menu").slideUp();
 
 });
