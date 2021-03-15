95d65dda811742075c9053e0cfee264372368b2e
Sergio <sergio.marretta@libero.it>
2021-03-15 18:56:37 +0100

    Complete

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,14 @@
 $(".main-nav li").mouseenter(function() {
-$(this).children(".dropdown-menu").show(400);
+$(this).children(".dropdown-menu").slideDown(400);
 
 });
-$(".main-nav li").mouseleave(function() {
+
+$(".main-nav li").click(function() {
 $(this).children(".dropdown-menu").slideUp(400);
 
 });
+
+// $(".main-nav li").mouseenter(function(){
+//   $(this).children("dropdown-menu").removeClass("dropdown-menu");
+//   $(this).children("dropdown-menu").addClass("dropdown-visible");
+// });
